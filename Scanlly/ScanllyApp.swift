b78614b052c9060886e5c0d13fc81cb907d13110
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-07-28 16:09:59 +0100

    🎉 the beginning

---
+++ Scanlly/ScanllyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanllyApp.swift
+//  Scanlly
+//
+//  Created by Godwin IE on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScanllyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
